eed10034d3225ddc4dc2d5a09210e26f48527c98
bendik0329 <razvanbendik0329@gmail.com>
2022-02-28 14:48:29 -0500

    trying to get headers to cache fully

--- api/comments.js
+++ api/comments.js
@@ -14,6 +14,11 @@ export default async function handler(request, res) {
           return t.json();
         }
       }).then((data) => data.record);
+      res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+      res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
       res.json(await comments);
     break;
   }
