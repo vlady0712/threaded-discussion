fce5e01cb977b97981b434395f528c3d1249d24f
bendik0329 <razvanbendik0329@gmail.com>
2022-03-24 20:02:15 -0400

    add starting api

--- api/submit-comment.js
+++ api/submit-comment.js
@@ -1,12 +1,21 @@
 import { PSDB } from 'planetscale-node';
+import { v4 as uuidv4 } from 'uuid';
+
+const conn = new PSDB('main');
 
 export default async function handler(req, res) {
-  const conn = new PSDB('main');
-  const [dbResult] = await conn.query('select * from users');
+  const reqBody = req.body;
   res.setHeader('Cache-Control', 'max-age=0, s-maxage=300');
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
-  res.json(await dbResult);
+  const commentUUID = uuidv4();
+  reqBody.uid = commentUUID;
+  reqBody.submitted_time = new Date().toISOString()
+  res.json(await JSON.stringify(reqBody));
+  conn.query('INSERT INTO comments(uid, thread_uid, submitted_time, body) VALUES ?', [[reqBody.uid, reqBody.thread_uid, reqBody.submitted_time, reqBody.body]], (err) => {
+    if (err) console.log("AHHHHHHHHHHHHHHHHHHHHH")
+    else console.log("Yay!")
+  });
 }
