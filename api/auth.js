ad834782a979021e4967ab84bde789254b61d1f2
bendik0329 <razvanbendik0329@gmail.com>
2022-03-31 14:51:36 -0400

    add auth api

---
+++ api/auth.js
@@ -0,0 +1,46 @@
+import pkg from 'jsonwebtoken';
+import * as mysql from 'mysql2/promise';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * @param {*} req 
+ * @param {*} res 
+ * 
+ * Generating crypto keys:
+ * require('crypto').randomBytes(64).toString('hex')
+ */
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    const accessTokenSecret = process.env.AUTH_ACCESS_TOKEN_SECRET;
+    const refreshTokenSecret = process.env.AUTH_REFRESH_TOKEN_SECRET;
+    const connection = await mysql.createConnection({
+        host: process.env.PLANETSCALE_DB_HOST,
+        user: process.env.PLANETSCALE_DB_USERNAME,
+        password: process.env.PLANETSCALE_DB_PASSWORD,
+        port: 3306,
+        database: process.env.PLANETSCALE_DB,
+        ssl: {}
+    });
+    const username = req.body.username;
+    const user = {
+        username: username,
+    };
+    const [rows] = await connection.query('SELECT * FROM users WHERE name = ?', [username]);
+ 
+    if (rows.length === 1){
+        user.uid = rows[0].uid;
+        user.isAdmin = rows[0].is_admin;
+    } else if (rows.length === 0){
+        user.uid = uuidv4();
+        user.isAdmin = false;
+        await connection.query('INSERT INTO users (uid, name, is_admin) VALUES (?,?,?)', [user.uid, user.username, user.isAdmin]);
+    } else {
+        // return some error
+    }
+    
+    const accessToken = pkg.sign(user, accessTokenSecret);
+    res.json({accessToken: accessToken});
+}
