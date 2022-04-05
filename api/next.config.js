876c321815cc044c33448afb34ec29a137798d66
bendik0329 <razvanbendik0329@gmail.com>
2022-04-05 12:10:15 -0400

    redirects added

--- api/next.config.js
+++ api/next.config.js
@@ -1,9 +1,30 @@
+// /api/comment/:uid GET && PUT && DELETE
+// /api/comment GET && POST
+// /api/thread POST
+// /api/user POST
+
 module.exports = {
-    async rewrites() {
+    async redirects() {
         return [
           {
-            source: '/api/like-comment',
-            destination: '/api/discussions/comment/like',
+            source: '/api/I-dont-like-Bryan/1',
+            destination: '/api/like-comment',
+          },
+          {
+            source: '/api/I-dont-like-Bryan/2',
+            destination: '/api/get-comment',
+          },
+          {
+            source: '/api/I-dont-like-Bryan/3',
+            destination: '/api/delete-comment',
+          },
+          {
+            source: '/api/I-dont-like-Bryan/4',
+            destination: '/api/submit-comment',
+          },
+          {
+            source: '/api/I-dont-like-Bryan/5',
+            destination: '/api/edit-comment',
           },
         ]
       },
