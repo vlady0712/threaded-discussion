19a79ec3b1ffb3d435b6da8a422a2b2f0dd3a6a8
bendik0329 <razvanbendik0329@gmail.com>
2022-04-07 12:09:25 -0400

    deleted files and such

--- api/create-user.js
+++ api/create-user.js
@@ -1,13 +1,15 @@
 // addUser.js
-import { connection } from "./dbConnection.js";
 import { v4 as uuidv4 } from 'uuid';
+import { connection } from "./dbConnection.js";
+
 
 export default async function handler(req, res) {
   console.log(`New User Endpoint: ${req.body}`)
   // this is making wide sweeping assumptions of the data accuracy
+  // eslint-disable-next-line camelcase
   const {is_admin, name} = JSON.parse(req.body);
   const uid = uuidv4();
-  let newUser = {"is_admin": is_admin, "name": name, "uid": uid};
+  const newUser = {"is_admin": is_admin, "name": name, "uid": uid};
 
   await connection.query('INSERT INTO users (uid, name, is_admin) VALUES (?,?,?)', [newUser.uid, newUser.name, newUser.is_admin]);
   res.json(newUser);
