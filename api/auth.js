8e007008644f4174cd72102a332e7de44961b4dd
bendik0329 <razvanbendik0329@gmail.com>
2022-04-07 12:14:46 -0400

    fix headers for preflight

--- api/auth.js
+++ api/auth.js
@@ -11,9 +11,13 @@ import { v4 as uuidv4 } from 'uuid';
  */
 export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8008");
     res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
+    if (req.method === 'OPTIONS'){
+        res.status(200).json();
+    }
     const accessTokenSecret = process.env.AUTH_ACCESS_TOKEN_SECRET;
     const refreshTokenSecret = process.env.AUTH_REFRESH_TOKEN_SECRET;
     const connection = await mysql.createConnection({
