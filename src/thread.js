1575291e9160c95a220f4f6bbca6f347ff4f2efb
bendik0329 <razvanbendik0329@gmail.com>
2022-04-25 15:37:01 -0400

    style and minor changes

--- src/thread.js
+++ src/thread.js
@@ -1,19 +1,12 @@
-// dependencies / things imported
 import { LitElement, html, css } from 'lit';
 import '@lrnwebcomponents/simple-icon/lib/simple-icons.js';
 import '@lrnwebcomponents/simple-icon/lib/simple-icon-lite.js';
 import '@lrnwebcomponents/simple-colors';
-
-// EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
-
-// which has the magic life-cycles and developer experience below added
 export class thread extends LitElement {
-  // a convention I enjoy so you can change the tag name in 1 place
   static get tag() {
     return 'image-prompt';
   }
 
-  // CSS - specific to Lit
   static get styles() {
     return css`
       :host {
@@ -82,8 +75,6 @@ export class thread extends LitElement {
     `;
   }
 
-  // overlay on div tag - wrap image in div & style div
-  // HTMLElement life-cycle, built in; use this for setting defaults
   constructor() {
     super();
     this.imgSrc = '';
@@ -93,7 +84,6 @@ export class thread extends LitElement {
     this.icon = '';
   }
 
-  // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
   static get properties() {
     return {
       ...super.properties,
@@ -105,8 +95,6 @@ export class thread extends LitElement {
     };
   }
 
-  // updated fires every time a property defined above changes
-  // this allows you to react to variables changing and use javascript to perform logic
   updated(changedProperties) {
     changedProperties.forEach((oldValue, propName) => {
       if (propName === 'status' && this[propName] === 'correct') {
@@ -123,27 +111,20 @@ export class thread extends LitElement {
     });
   }
 
-  // Lit life-cycle; this fires the 1st time the element is rendered on the screen
-  // this is a sign it is safe to make calls to this.shadowRoot
   firstUpdated(changedProperties) {
     if (super.firstUpdated) {
       super.firstUpdated(changedProperties);
     }
   }
 
-  // HTMLElement life-cycle, element has been connected to the page / added or moved
-  // this fires EVERY time the element is moved
   connectedCallback() {
     super.connectedCallback();
   }
 
-  // HTMLElement life-cycle, element has been removed from the page OR moved
-  // this fires every time the element moves
   disconnectedCallback() {
     super.disconnectedCallback();
   }
 
-  // HTML - specific to Lit
   render() {
     return html`
       <div id="Nest">
@@ -165,11 +146,6 @@ export class thread extends LitElement {
     `;
   }
 
-  // HAX specific callback
-  // This teaches HAX how to edit and work with your web component
-  /**
-   * haxProperties integration via file reference
-   */
   static get haxProperties() {
     return new URL(`../lib/FlashCard.haxProperties.json`, import.meta.url).href;
   }
