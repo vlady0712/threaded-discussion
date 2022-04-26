a21f0f2bcf693a8b6ac2de91e2bcf8c35b21bab4
bendik0329 <razvanbendik0329@gmail.com>
2022-04-26 00:35:33 -0400

    create-thread api

--- api/create-thread.js
+++ api/create-thread.js
@@ -1,10 +1,20 @@
+import { connection } from "./_dbConnection.js";
+
 /**
  * 
  * @param {Object} request 
  * @param {Object} response 
  */
-export default function handler(request, response) {
+export default async function handler(request, response) {
     // the thread ID should be set as the hash of the page URL
+    /*  UID
+      const currentPage = window.location.href;
+      const hashBits = sjcl.hash.sha256.hash(currentPage);
+      return sjcl.codec.hex.fromBits(hashBits);
+
+        DOMAIN
+      window.location.hostname
+    /*
     // the response should include permissions for the thread as chmod (AGE:admin,group,everyone)
     /*
         0: (000) No permission.
@@ -16,9 +26,13 @@ export default function handler(request, response) {
         6: (110) Read and write permissions.
         7: (111) Read, write, and execute permissions.
     */
-    response.status(200).json({
-      body: request.body,
-      query: request.query,
-      cookies: request.cookies,
-    });
+    const reqBody = JSON.parse(request.body);
+    console.log(reqBody);
+    if (reqBody.uid && reqBody.domain && reqBody.permissions){
+      const queryResponse = await connection.query('INSERT INTO threads (uid, domain, permissions) VALUES (?,?,?)', [reqBody.uid, reqBody.domain, reqBody.permissions]);
+      response.status(200).json(queryResponse);
+    } else {
+      response.status(400).send("invalid request parameters")
+    }
+    
 }
